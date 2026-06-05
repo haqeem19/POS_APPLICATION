a2207623cd50c885997ade373038ab4f20035d34
Haqeem Luqman <haqeem19@users.noreply.github.com>
2026-06-05 12:42:50 +0700

    Fix Laravel view binding for Vercel

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,12 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\ViewServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->app->register(ViewServiceProvider::class);
     }
 
     public function boot(): void
